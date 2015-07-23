0f46680b3f26859c4ffea697827b27b37016734e
Bruno Gomes <blfgomes@gmail.com>
2015-07-23 17:50:02 -0300

    Changing vmo to a single instance resource

--- lib/puppet/provider/vmo/vmo.rb
+++ lib/puppet/provider/vmo/vmo.rb
@@ -1,13 +1,35 @@
 require 'puppet/provider/tunable'
 
-Puppet::Type.type(:vmo).provide :vmo, :parent => Puppet::Provider::Tunable do
+Puppet::Type.type(:vmo).provide :vmo do
   confine :operatingsystem => :AIX
-  commands :cmd => 'vmo'
+  commands :vmo => 'vmo'
 
   mk_resource_methods
-  
-  def value=(v)
-    set_value(resource[:name], v)
+
+  def self.instances
+    attr_hash = {:name => 'vmo'}
+    vmo('-x').split("\n").each do |line|
+      name, value = line.split(',')[0..1]
+      name.sub!(/%$/, '_p')
+      attr_hash[name.to_sym] = value
+    end
+    [new(attr_hash)]
   end
+
+  def flush
+    vmo_properties = []
+    resource.to_hash.each do |attr, value|
+      next if ! self.class.resource_type.validproperties.include? attr
+      attr_str = attr.to_s.sub(/_p$/, '%')
+      vmo_properties << ['-o', "#{attr_str}=#{value}"]
+    end
+    begin
+        vmo(vmo_properties)
+    rescue Puppet::ExecutionFailure => e
+      @property_hash = {}
+      raise Puppet::Error, "vmo had an error -> #{e.inspect}"
+    end
+  end
+
 
 end 
--- lib/puppet/type/vmo.rb
+++ lib/puppet/type/vmo.rb
@@ -1,11 +1,132 @@
 Puppet::Type.newtype(:vmo) do
 
   newparam(:name, :namevar => true) do
-    desc "Name of tunable"
   end
 
-  newproperty(:value) do
-    desc "Value of tunable"
+  newproperty(:ame_cpus_per_pool) do
+  end
+
+  newproperty(:ame_maxfree_mem) do
+  end
+
+  newproperty(:ame_min_ucpool_size) do
+  end
+
+  newproperty(:ame_minfree_mem) do
+  end
+
+  newproperty(:ams_loan_policy) do
+  end
+
+  newproperty(:enhanced_affinity_affin_time) do
+  end
+
+  newproperty(:enhanced_affinity_vmpool_limit) do
+  end
+
+  newproperty(:esid_allocator) do
+  end
+
+  newproperty(:force_relalias_lite) do
+  end
+
+  newproperty(:kernel_heap_psize) do
+  end
+
+  newproperty(:lgpg_regions) do
+  end
+
+  newproperty(:lgpg_size) do
+  end
+
+  newproperty(:low_ps_handling) do
+  end
+
+  newproperty(:maxfree) do
+  end
+
+  newproperty(:maxperm) do
+  end
+
+  newproperty(:maxpin) do
+  end
+
+  newproperty(:maxpin_p) do
+  end
+
+  newproperty(:memory_frames) do
+  end
+
+  newproperty(:memplace_data) do
+  end
+
+  newproperty(:memplace_mapped_file) do
+  end
+
+  newproperty(:memplace_shm_anonymous) do
+  end
+
+  newproperty(:memplace_shm_named) do
+  end
+
+  newproperty(:memplace_stack) do
+  end
+
+  newproperty(:memplace_text) do
+  end
+
+  newproperty(:memplace_unmapped_file) do
+  end
+
+  newproperty(:minfree) do
+  end
+
+  newproperty(:minperm) do
+  end
+
+  newproperty(:minperm_p) do
+  end
+
+  newproperty(:nokilluid) do
+  end
+
+  newproperty(:npskill) do
+  end
+
+  newproperty(:npswarn) do
+  end
+
+  newproperty(:num_locks_per_semid) do
+  end
+
+  newproperty(:numpsblks) do
+  end
+
+  newproperty(:pinnable_frames) do
+  end
+
+  newproperty(:relalias_percentage) do
+  end
+
+  newproperty(:scrub) do
+  end
+
+  newproperty(:thrpgio_inval) do
+  end
+
+  newproperty(:thrpgio_npages) do
+  end
+
+  newproperty(:v_pinshm) do
+  end
+
+  newproperty(:vmm_default_pspa) do
+  end
+
+  newproperty(:vmm_klock_mode) do
+  end
+
+  newproperty(:wlm_memlimit_nonpg) do
   end
 
 end
