85163df3ae17621c3464a39179e523823890d4f2
Bruno Gomes <blfgomes@gmail.com>
2015-07-27 13:03:53 -0300

    Code for ioo

--- lib/puppet/type/ioo.rb
+++ lib/puppet/type/ioo.rb
@@ -1,11 +1,172 @@
+require 'puppet/type/aix_tunables_util'
+
 Puppet::Type.newtype(:ioo) do
+  include Tunables_Util
 
   newparam(:name, :namevar => true) do
     desc "Name of tunable"
   end
 
-  newproperty(:value) do
-    desc "Value of tunable"
+  newproperty(:aio_maxreqs) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+  newproperty(:aio_maxservers) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:aio_minservers) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:aio_server_inactivity) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_atimeUpdateSymlink) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_dynamicBufferPreallocation) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_inodeCacheSize) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_maxPageReadAhead) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_maxRandomWrite) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_metadataCacheSize) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_minPageReadAhead) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_nPagesPerWriteBehindCluster) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_nRandomCluster) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_recoveryMode) do
+    munge do |value|
+      puts value
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_syncPageCount) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:j2_syncPageLimit) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:lvm_bufcnt) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:maxpgahead) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:maxrandwrt) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:numclust) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:numfsbufs) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:pd_npages) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:posix_aio_active) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:posix_aio_maxreqs) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:posix_aio_maxservers) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:posix_aio_minservers) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
+  end
+
+  newproperty(:posix_aio_server_inactivity) do
+    munge do |value|
+      @resource.munge_default(name, value)
+    end
   end
 
 end
