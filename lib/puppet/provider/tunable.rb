3e362e6fd95268ce53e768fa970f505d94971175
Bruno Gomes <blfgomes@gmail.com>
2015-07-27 13:03:15 -0300

    Code for handling case sensitive parameter names in kernal tunables

--- lib/puppet/provider/tunable.rb
+++ lib/puppet/provider/tunable.rb
@@ -17,13 +17,12 @@ class Puppet::Provider::Tunable < Puppet::Provider
     begin
       instances_hash = class_variable_get(:@@instances_hash)
     rescue 
-      #self.instances
       cmd('-x').split("\n").each do |line|
 	line_array = line.split(',')
 	name, current, default = line_array[0..3]
 	type = line_array[-2]
 	name.sub!(/%$/, '_p')
-	instances_hash[name] = TunableProperty.new(name, current, default, type)
+	instances_hash[name.downcase] = TunableProperty.new(name, current, default, type)
       end
       class_variable_set(:@@instances_hash, instances_hash)
     end
@@ -35,7 +34,10 @@ class Puppet::Provider::Tunable < Puppet::Provider
     instances_hash = self.class.instances_hash
     resource.to_hash.each do |attr, value|
       next if ! self.class.resource_type.validproperties.include? attr
-      attr_str = attr.to_s
+      # Properties in self.class.resource are stored lowercase while Kernel
+      # tunables in AIX are case sensitive.
+      # We should use the original name stored in TunableProperty.@name
+      attr_str = instances_hash[attr.to_s].name
       attr_str.sub!(/_p$/, '%')
       cmd_properties << ['-o', "#{attr_str}=#{value}"]
     end
