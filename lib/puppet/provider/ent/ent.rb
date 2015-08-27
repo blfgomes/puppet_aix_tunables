4c899e1a1b96fc106486b8d1a90667872f6ae03e
Bruno Gomes <blfgomes@gmail.com>
2015-08-27 18:40:26 -0300

    Added ent type (Virtual I/O Ethernet Adapter)

---
+++ lib/puppet/provider/ent/ent.rb
@@ -0,0 +1,56 @@
+Puppet::Type.type(:ent).provide :ent do
+  confine :operatingsystem => :AIX
+  commands :lsattr => 'lsattr'
+  commands :chdev  => 'chdev'
+  commands :lsdev  => 'lsdev'
+
+  mk_resource_methods
+  
+  def self.create_attr_hash(attr_lines)
+    attr_hash = {}
+    attr_lines.each do |line|
+      attribute, value = line.split(',')
+      attr_hash[attribute.to_sym] = value
+    end
+    return attr_hash
+  end
+
+  def self.instances
+    ents = lsdev('-Ct', 'IBM,l-lan').split("\n").collect do |line|
+      line.split(/\s+/)[0]
+    end
+    ents.collect do |ent|
+      attr_lines = lsattr('-F', 'attribute,value', '-l', ent).split("\n")
+      attr_hash = create_attr_hash(attr_lines)
+      attr_hash[:name] = ent
+      new(attr_hash)
+    end
+  end
+
+  def self.prefetch(resources)
+    instances.each do |prov|
+      if resource = resources[prov.name]
+        resource.provider = prov
+      end
+    end
+  end
+
+  def flush
+    options = []
+    resource.to_hash.each do |attr, value|
+      next if ! self.class.resource_type.validproperties.include? attr
+      options << ['-a', "#{attr}=#{value}"] 
+    end
+    begin
+      if resource[:device_in_use] == :true
+	chdev('-l', resource[:name], options, '-P')
+      else
+	chdev('-l', resource[:name], options)
+      end
+    rescue Puppet::ExecutionFailure => e
+      @property_hash = {}
+      raise Puppet::Error, "chdev had an error -> #{e.inspect}"
+    end
+  end
+
+end 
---
+++ lib/puppet/type/ent.rb
@@ -0,0 +1,61 @@
+Puppet::Type.newtype(:ent) do
+
+  newparam(:name, :namevar => true) do
+    desc "Name of Virtual Ethernet Adapter"
+  end
+
+  newproperty(:alt_addr) do
+  end
+
+  newproperty(:chksum_offload) do
+  end
+
+  newproperty(:copy_buffs) do
+  end
+
+  newproperty(:copy_bytes) do
+  end
+
+  newproperty(:max_buf_huge) do
+  end
+
+  newproperty(:max_buf_large) do
+  end
+
+  newproperty(:max_buf_medium) do
+  end
+
+  newproperty(:max_buf_small) do
+  end
+
+  newproperty(:max_buf_tiny) do
+  end
+
+  newproperty(:min_buf_huge) do
+  end
+
+  newproperty(:min_buf_large) do
+  end
+
+  newproperty(:min_buf_medium) do
+  end
+
+  newproperty(:min_buf_small) do
+  end
+
+  newproperty(:min_buf_tiny) do
+  end
+
+  newproperty(:trace_debug) do
+  end
+
+  newproperty(:use_alt_addr) do
+  end
+
+  newparam(:device_in_use) do
+    defaultto :true
+    newvalues(:true, :false)
+  end
+
+
+end
